db71666036f732a3520c765a0ba726de14d2f9af
임민영 <raccoon@shinsegae.com>
2025-12-17 14:05:58 +0900

    fix: Update Vite base path configuration and HTML asset references.

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/text2sql_pilotreport/',
   build: {
     outDir: 'docs',
   },
